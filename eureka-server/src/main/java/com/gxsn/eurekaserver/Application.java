04c982e2f96a8a1061696a7291eac460307c7259
jiangbaowei2 <15801277037@126.com>
2018-03-29 13:25:03 +0800

    ceshi

---
+++ eureka-server/src/main/java/com/gxsn/eurekaserver/Application.java
@@ -0,0 +1,11 @@
+package com.gxsn.eurekaserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+public class Application {
+    public static void main(String[] args){
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
